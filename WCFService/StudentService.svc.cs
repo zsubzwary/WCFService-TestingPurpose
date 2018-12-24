edbdc1fd6467c1f30af7b5b5b5043626f226c910
zakawat <31027252+zakawat@users.noreply.github.com>
2018-12-24 21:17:07 +0500

    Error handeled of invalid ID on updatingStudent

--- WCFService/StudentService.svc.cs
+++ WCFService/StudentService.svc.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
+using System.ServiceModel.Activation;
 using System.Text;
 
 namespace WCFService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "StudentService" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select StudentService.svc or StudentService.svc.cs at the Solution Explorer and start debugging.
+
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
     public class StudentService : IStudentService
     {
         private static spDatabase database = new spDatabase();
@@ -44,6 +47,10 @@ namespace WCFService
         public Student updateStudentInDB(int id, string name, int age)
         {
             Student student = database.Students.Where(s => s.id == id).FirstOrDefault();
+            if (student == null)
+            {
+                return new Student() { name = "" , id = -1, age = -1  };
+            }
             student.name = name;
             student.age = age;
 
